32ae54c108e4c641cb4d1274e3a045072fc84f82
balhar <root@vps.frenzy.cz>
2014-02-23 00:16:31 +0100

    Prvni funkcni prototyp generovani dat pro js graf z databaze

--- meteo.py
+++ meteo.py
@@ -1,6 +1,8 @@
 import web
 import os
 import sys
+import json
+import datetime
 
 # pridani cesty ke knihovnam
 lib_path = os.path.abspath('./libs')
@@ -10,16 +12,27 @@ sys.path.append(lib_path)
 rootdir = os.path.abspath(os.path.dirname(__file__)) + '/'
 render = web.template.render(rootdir + 'templates/', base='layout', cache=False)
 
+db = web.database(dbn='sqlite', db=rootdir + 'database/database.sqlite')
+
 urls = (
     '/', 'index'
 )
 
 ########################################################################
 
-# Hlavni stranka pro vyhledani a nahlaseni IP adresy
+# Hlavni stranka
 class index:
   def GET(self):
-    return render.index()
+    series = []
+    result = db.query("SELECT DISTINCT position FROM records;")
+    for row in result:
+      name = row['position']
+      serie = {'name': name, 'data':[]}
+      data = db.select('records', what="date,tempreature", where='position = $position', vars={'position':name}, order="date DESC", limit=120)
+      for record in reversed(data.list()):
+        serie['data'].append([int(datetime.datetime.strptime(record['date'], "%Y-%m-%d %H:%M:%S").strftime('%s')) * 1000, record['tempreature']])
+      series.append(serie)
+    return render.index(json.dumps(series))
 
 ########################################################################
     
--- static/js/graphs.js
+++ static/js/graphs.js
@@ -3,7 +3,7 @@
     $('.graph').each(function () {
       $(this).highcharts({
           chart: {
-              type: 'line'
+              type: $(this).data('type')
           },
           title: {
               text: $(this).data('title')
@@ -12,27 +12,12 @@
               title: {
                   text: $(this).data('ytitle')
               }
-          },{
-              title: {
-                  text: $(this).data('y2title')
-              },
-              opposite: true,
-              linkedTo: 0,
-              tickInterval: 10
           }
           ],
-          series: [
-          {
-              name: 'Tempreature',
-              yaxis: 0,
-              data: [1, 0, 4,4,4,4,4,4,3,4,3,2,3,4,5,6,5,4,3,4,5,4,3,5,4,3,4]
+          xAxis: {
+              type: 'datetime'
           },
-          {
-              name: 'Humidity',
-              yaxis: 1,
-              data: [10, 10, 40,40,40,40,40,40,30,40,30,20,30,40,50,60,50,40,30,40,50,40,30,50,40,30,40]
-          }
-          ]
+          series: $(this).data('series')
     });
   });
   });
