82859af8bdf7242967f7b06da6b88d2f1747d41f
balhar <root@vps.frenzy.cz>
2014-02-23 21:06:17 +0100

    Spousta vylepseni, dodelane odkazy na zpetne prohlizeni dat, oprava adres statisckych souboru, menu ad.

--- meteo.py
+++ meteo.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 import datetime
+import time
 
 # pridani cesty ke knihovnam
 lib_path = os.path.abspath('./libs')
@@ -15,26 +16,48 @@ render = web.template.render(rootdir + 'templates/', base='layout', cache=False)
 db = web.database(dbn='sqlite', db=rootdir + 'database/database.sqlite')
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/last/(.+)', 'index'
 )
 
 ########################################################################
 
-# Hlavni stranka
+# Stranka s poslednim merenim
 class index:
-  def GET(self):
+  def GET(self, filter=None):
+    # filtrace zobrazenych hodnot dle url
+    limit = ''
+    where = ''
+    if filter == 'now' or filter == None:
+      limit = 'LIMIT 120'
+      where = '1'
+    if filter == 'today':
+      today = time.strftime('%Y-%m-%d') + ' %'
+      where = 'date like "%s"' % (today)
+    if filter == 'yesterday':
+      yesterday = datetime.date.today() - datetime.timedelta(days=1)
+      yesterday = yesterday.strftime ("%Y-%m-%d") + ' %'
+      where = 'date like "%s"' % (yesterday)
+    if filter == 'week':
+      today = time.strftime('%Y-%m-%d') + ' 23:59:59'
+      weekback = datetime.date.today() - datetime.timedelta(days=7)
+      weekback = weekback.strftime ("%Y-%m-%d") + ' 00:00:00'
+      where = 'date between "%s" and "%s"' % (weekback, today)
+
     # Prazdne pole pro serie dat grafu a maxima a minima senzeoru
     temp_series = []
     sensors = []
     # Vybereme vsechny cidla z databaze a vytvorime serii pro kazde z nich
-    result = db.query("SELECT DISTINCT position FROM records ORDER BY date, position DESC LIMIT 120;")
+    print "filter %s where %s limit %s" % (filter, where, limit)
+    print "SELECT DISTINCT position FROM records WHERE %s ORDER BY date, position DESC %s;" % (where, limit)
+    result = db.query("SELECT DISTINCT position FROM records WHERE %s ORDER BY date, position DESC %s;" % (where, limit))
     for row in result:
       name = row['position']
       # Zakladni struktura kazde serie
       serie = {'name': name, 'data':[]}
       sensor = {'name': name, 'min': 1000, 'max': -1000}
       # vybereme data jen pro konkretni senzor
-      data = db.select('records', what="date,tempreature", where='position = $position', vars={'position':name}, order="date DESC", limit=120)
+      data = db.query('SELECT date, tempreature FROM records where position = "%s" and %s ORDER BY date DESC %s' % (name, where, limit))
       # data musi byt setrizena - proto prevod na list a trizeni
       for record in reversed(data.list()):
         serie['data'].append([int(datetime.datetime.strptime(record['date'], "%Y-%m-%d %H:%M:%S").strftime('%s')) * 1000, record['tempreature']])
@@ -53,7 +76,7 @@ class index:
 
     #Conditions
     cond_serie = []
-    data = db.query('select condition, count(condition) as count from (select condition from records where condition != "Inside" limit 120) group by condition;')
+    data = db.query('select condition, count(condition) as count from (select condition from records where condition != "Inside" and %s %s) group by condition;' % (where, limit))
     # data musi byt setrizena - proto prevod na list a trizeni
     for record in data:
       cond_serie.append([record['condition'], record['count']])
