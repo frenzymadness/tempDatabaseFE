c894dea6a451294111de4dd48fd527dc550129a2
frenzymadness <luu@test.cz>
2014-02-24 09:41:56 +0100

    Upravena intendace jQuery.

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,41 +1,41 @@
 (function($){
   $(document).ready(function() {
-  Highcharts.setOptions({
-    global: {
+    Highcharts.setOptions({
+      global: {
         timezoneOffset: -1 * 60
-            }
+      }
     });
     $('.graphline').each(function () {
       $(this).highcharts({
-          chart: {
-              type: 'line'
-          },
+        chart: {
+          type: 'line'
+        },
+        title: {
+          text: $(this).data('title')
+        },
+        yAxis: [{
           title: {
-              text: $(this).data('title')
-          },
-          yAxis: [{
-              title: {
-                  text: $(this).data('ytitle')
-              }
+            text: $(this).data('ytitle')
           }
-          ],
-          xAxis: {
-              type: 'datetime'
-          },
-          series: $(this).data('series')
-    });
+        }
+        ],
+        xAxis: {
+          type: 'datetime'
+        },
+        series: $(this).data('series')
+      });
     });
     $('.graphpie').each(function () {
       $(this).highcharts({
-          chart: {
-              type: $(this).data('type')
-          },
-          title: {
-              text: $(this).data('title')
-          },
-          series: [{ type: 'pie',
-                    data: $(this).data('series') }]
+        chart: {
+          type: $(this).data('type')
+        },
+        title: {
+          text: $(this).data('title')
+        },
+        series: [{ type: 'pie',
+        data: $(this).data('series') }]
+      });
     });
-  });
   });
 })(jQuery);
